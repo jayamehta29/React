f4e5d797a3f3e1c467368500f144ebc1f390dedd
Jaya <jayamehta29@gmail.com>
2021-09-24 01:57:02 +0530

    carousal

--- binge/src/Components/CustomCarousal.jsx
+++ binge/src/Components/CustomCarousal.jsx
@@ -34,29 +34,40 @@ const CustomCarousal = () => {
               <p>We had such a great time in LA!</p>
             </div>
           </div>
-
-
-
-
-
-
-
-
-
-
-
           <div className="carousel-item">
+            <div className="left-crsl">
+              <h2 style={{ color: "white" }}>NAME</h2>
+              <div className="txt" style={{ width: "50%", }}>
+                <div className="text-md-left">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat repudiandae, alias nihil suscipit assumenda consectetur fugit voluptatum maxime harum consequuntur natus voluptates cumque minus iusto soluta, dolor possimus velit sint!</div>
+              </div>
+              <div className="btns">
+                <button className="cbtn diff-btn"><FontAwesomeIcon icon={faPlay} style={{marginRight:"10px"}}/>Watch Now</button>
+                <button className="cbtn">watchlist</button>
+                <button className="cbtn">details</button>
+              </div>
+            </div>
             <img src="https://cdn.pixabay.com/photo/2021/04/23/19/57/yorkshire-terrier-6202621_960_720.jpg" className="d-block cimg" style={{ width: "35%" }} />
             <div className="carousel-caption" style={{ color: "black", width: "15rem", marginLeft: "62%" }}>
-              <h3>Chicago</h3>
-              <p>Thank you, Chicago!</p>
+              <h3>Los Angeles</h3>
+              <p>We had such a great time in LA!</p>
             </div>
           </div>
           <div className="carousel-item">
+            <div className="left-crsl">
+              <h2 style={{ color: "white" }}>NAME</h2>
+              <div className="txt" style={{ width: "50%", }}>
+                <div className="text-md-left">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat repudiandae, alias nihil suscipit assumenda consectetur fugit voluptatum maxime harum consequuntur natus voluptates cumque minus iusto soluta, dolor possimus velit sint!</div>
+              </div>
+              <div className="btns">
+                <button className="cbtn diff-btn"><FontAwesomeIcon icon={faPlay} style={{marginRight:"10px"}}/>Watch Now</button>
+                <button className="cbtn">watchlist</button>
+                <button className="cbtn">details</button>
+              </div>
+            </div>
             <img src="https://cdn.pixabay.com/photo/2021/04/23/19/57/yorkshire-terrier-6202621_960_720.jpg" className="d-block cimg" style={{ width: "35%" }} />
             <div className="carousel-caption" style={{ color: "black", width: "15rem", marginLeft: "62%" }}>
-              <h3>New York</h3>
-              <p>We love the Big Apple!</p>
+              <h3>Los Angeles</h3>
+              <p>We had such a great time in LA!</p>
             </div>
           </div>
         </div>
